23c5a82e4ad7076a90a37fb3d24a0ee3a5ce4607
DiSalvio <stephendisalvio@protonmail.com>
2022-03-18 13:07:39 +0800

    Add soft 404 page and navigation guards

---
+++ forum/src/components/PageNotFound.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="col-full text-center">
+    <h1>The page you're looking for doesn't exist</h1>
+    <h1><router-link :to="{name: 'PageHome'}">Go Home</router-link></h1>
+  </div>
+</template>
