983230cac83e2e102cae6bbc3d45ac922ff9d225
DiSalvio <stephendisalvio@protonmail.com>
2022-03-19 12:42:46 +0800

    split post list logic into separate component

---
+++ forum/src/components/PostList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="post-list">
+    <div
+      v-for="post in posts"
+      :key="post.id"
+      class="post"
+    >
+
+      <div class="user-info">
+
+        <a href="#" class="user-name">
+          -{{findUserById(post.userId).name}}
+        </a>
+
+        <a href="#">
+          <img class="avatar-large" :src="findUserById(post.userId).avatar">
+        </a>
+
+      </div>
+
+      <div class="post-content">
+        <div>
+          <p>{{post.text}}</p>
+        </div>
+      </div>
+
+      <div class="post-date text-faded">
+        {{post.publishedAt}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+export default {
+  props: {
+    posts: {
+      required: true,
+      type: Array
+    }
+  },
+  data () {
+    return {
+      users: sourceData.users
+    }
+  },
+  methods: {
+    findUserById (postUserId) {
+      return this.users.find(user => user.id === postUserId)
+    }
+  }
+}
+</script>
--- forum/src/pages/PageThreadShow.vue
+++ forum/src/pages/PageThreadShow.vue
@@ -1,49 +1,21 @@
 <template>
   <div class="col-large push-top">
-    <h1>{{thread.title }}</h1>
-    <div class="post-list">
-
-      <div
-        v-for="postId in thread.posts"
-        :key="postId"
-        class="post"
-      >
-
-       <div class="user-info">
-
-          <a href="#" class="user-name">
-            -{{findUserById(findPostById(postId).userId).name}}
-          </a>
-
-          <a href="#">
-            <img class="avatar-large" :src="findUserById(findPostById(postId).userId).avatar">
-          </a>
-
-        </div>
-
-        <div class="post-content">
-          <div>
-            <p>{{findPostById(postId).text}}</p>
-          </div>
-        </div>
-
-        <div class="post-date text-faded">
-          {{findPostById(postId).publishedAt}}
-        </div>
-
-      </div>
-    </div>
+    <h1>{{ thread.title }}</h1>
+    <post-list :posts="threadPosts"/>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data.json'
+import PostList from '@/components/PostList.vue'
 export default {
+  components: {
+    PostList
+  },
   data () {
     return {
       threads: sourceData.threads,
-      posts: sourceData.posts,
-      users: sourceData.users
+      posts: sourceData.posts
     }
   },
   props: {
@@ -55,14 +27,9 @@ export default {
   computed: {
     thread () {
       return this.threads.find(thread => thread.id === this.id)
-    }
-  },
-  methods: {
-    findPostById (postId) {
-      return this.posts.find(p => p.id === postId)
     },
-    findUserById (userId) {
-      return this.users.find(u => u.id === userId)
+    threadPosts () {
+      return this.posts.filter(post => post.threadId === this.id)
     }
   }
 }
