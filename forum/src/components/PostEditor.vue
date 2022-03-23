e131b59f514b903d3967bfc9fe8e2fd652896981
DiSalvio <stephendisalvio@protonmail.com>
2022-03-23 14:04:36 +0800

    Add post editor

---
+++ forum/src/components/PostEditor.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="col-full">
+    <form @submit.prevent="savePost">
+      <div class="form-group">
+        <textarea v-model="newPostText" name="" id="" cols="30" rows="10" class="form-input"/>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">Submit Post</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      newPostText: ''
+    }
+  },
+  methods: {
+    savePost () {
+      const post = {
+        postId: 'gggg' + Math.random(),
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        userId: '38St7Q8Zi2N1SPa5ahzssq9kbyp1'
+      }
+      this.$emit('savePost', { post })
+      this.newPostText = ''
+    }
+  }
+}
+</script>
