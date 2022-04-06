66052b2284155fdd97b23033718d902efae5cbe9
DiSalvio <stephendisalvio@protonmail.com>
2022-04-06 10:16:33 +0800

    extract thread form into separate component and use it on add thread page

---
+++ forum/src/components/ThreadEditor.vue
@@ -0,0 +1,37 @@
+<template>
+  <form @submit.prevent="saveThread">
+    <div class="form-group">
+      <label for="thread_title">Title:</label>
+      <input v-model="newThreadTitle" type="text" id="thread_title" class="form-input" name="title" >
+    </div>
+
+    <div class="form-group">
+      <label for="thread_content">Content:</label>
+      <textarea v-model="newThreadText" id="thread_content" class="form-input" name="content" rows="8" cols="140"></textarea>
+    </div>
+
+    <div class="btn-group">
+      <button @click="cancelSaveThread" class="btn btn-ghost">Cancel</button>
+      <button class="btn btn-blue" type="submit" name="Publish">Publish </button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      newThreadTitle: '',
+      newThreadText: ''
+    }
+  },
+  methods: {
+    saveThread () {
+      this.$emit('saveThread', { title: this.newThreadTitle, text: this.newThreadText })
+    },
+    cancelSaveThread () {
+      this.$emit('cancelSaveThread', {})
+    }
+  }
+}
+</script>
--- forum/src/pages/PageThreadAdd.vue
+++ forum/src/pages/PageThreadAdd.vue
@@ -3,37 +3,20 @@
 
     <h1>Create new thread in <i>{{ forum.name }}</i></h1>
 
-    <form @submit.prevent="saveThread">
-      <div class="form-group">
-        <label for="thread_title">Title:</label>
-        <input v-model="newThreadTitle" type="text" id="thread_title" class="form-input" name="title" >
-      </div>
-
-      <div class="form-group">
-        <label for="thread_content">Content:</label>
-        <textarea v-model="newThreadText" id="thread_content" class="form-input" name="content" rows="8" cols="140"></textarea>
-      </div>
-
-      <div class="btn-group">
-        <button @click="cancelAddThread" class="btn btn-ghost">Cancel</button>
-        <button class="btn btn-blue" type="submit" name="Publish">Publish </button>
-      </div>
-    </form>
+    <ThreadEditor @saveThread="saveThread" @cancelSaveThread="cancelSaveThread"/>
   </div>
 </template>
 
 <script>
+import ThreadEditor from '@/components/ThreadEditor.vue'
 export default {
+  components: {
+    ThreadEditor
+  },
   props: {
     forumId: {
       required: true,
       type: String
-    }
-  },
-  data () {
-    return {
-      newThreadTitle: '',
-      newThreadText: ''
     }
   },
   computed: {
@@ -42,15 +25,15 @@ export default {
     }
   },
   methods: {
-    async saveThread () {
+    async saveThread ({ title, text }) {
       const thread = await this.$store.dispatch('createThread', {
-        title: this.newThreadTitle,
-        text: this.newThreadText,
+        title,
+        text,
         forumId: this.forumId
       })
       this.$router.push({ name: 'PageThreadShow', params: { id: thread.id } })
     },
-    cancelAddThread () {
+    cancelSaveThread () {
       this.$router.push({ name: 'ForumShow', params: { id: this.forumId } })
     }
   }
