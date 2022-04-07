02ceda13c780362103be234b817adb1800f9e4ce
DiSalvio <stephendisalvio@protonmail.com>
2022-04-07 13:16:28 +0800

    Remove console logs

--- forum/src/helpers/index.js
+++ forum/src/helpers/index.js
@@ -1,8 +1,6 @@
 export const findById = (resource, id) => resource.find(r => r.id === id)
 
 export const replaceItemInArray = (resource, item) => {
-  console.log('resource: ' + resource)
-  console.log('item: ' + item)
   resource[resource.findIndex(r => r.id === item.id)] = item
 }
 
